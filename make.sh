f26f328e26074bd2797a3bb6de50ed89d53f3b80
FuG <fu.gary.2009@gmail.com>
2012-11-01 12:06:14 -0700

    removed make.sh

--- make.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-g++ Library/mysql_interface.cpp Testing/driver.cpp Testing/unit_tests.cpp libmysqlcppconn.so
-g++ -c Library/mysql_interface.cpp
-mv mysql_interface.o dist/lib/
-cp Library/mysql_interface.h dist/include/
